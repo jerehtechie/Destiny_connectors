8d8742cce4bccede4bbf2835eb1ecc1bc0d4eee2
Jeremia Gathuru <jeremymburu0@gmail.com>
2026-03-10 00:42:32 +0300

    Initial commit: Destiny Connector React + Vite app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import DestinyConnector from './DestinyConnector';
+
+const App = () => {
+  return (
+    <div>
+     <DestinyConnector /> 
+    </div>
+  )
+}
+
+export default App
